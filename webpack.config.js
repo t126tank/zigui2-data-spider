4654c2a5d8cb856f5c533b72ffce90fd0dff4b31
shmily <go4thx@gmail.com>
2016-10-05 00:49:07 +0900

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
+var debug = process.argv.indexOf('-p') === -1;
 var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
   target: "node",
   context: path.join(__dirname, "src"),
-  devtool: "inline-sourcemap",
+  devtool: debug ? "inline-sourcemap" : null,
   entry: "./app.js",
   module: {
     loaders: [
@@ -24,5 +25,15 @@ module.exports = {
     path: __dirname + "/dist/",
     libraryTarget: "commonjs2",
     filename: "app.min.js"
-  }
+  },
+  plugins: debug ? [] : [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ],
 };
