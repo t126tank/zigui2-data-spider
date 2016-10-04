6b7683e6b39a187c921174e81ceaf55651df8633
fanliming <go4thx@gmail.com>
2016-10-04 18:39:12 +0900

    'init'

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+var path = require('path');
+var nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  target: "node",
+  context: path.join(__dirname, "src"),
+  devtool: debug ? "inline-sourcemap" : null,
+  entry: "./app.js",
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'stage-0'],
+          plugins: ['transform-decorators-legacy'],
+        }
+      }
+    ]
+  },
+  externals: /^[a-z\-0-9]+$/,
+  output: {
+    path: __dirname + "/dist/",
+    libraryTarget: "commonjs2",
+    filename: "app.min.js"
+  },
+  plugins: debug ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ],
+};
